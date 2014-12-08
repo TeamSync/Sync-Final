f5291be8d3ac8b8f625378b6645fb0ea49fbf010
mfedronic <mfedronic@gmail.com>
2014-12-07 22:40:24 -0500

    Update TextSender.java
    Renaming "TextSender.java" to "SendVideos.java"

--- src/com/syncteam/sync/TextSender.java
+++ src/com/syncteam/sync/TextSender.java
@@ -12,7 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class TextSender extends Activity {
+public class SendVideos extends Activity {
 
 	Button button;
 	EditText editPhoneNum;
