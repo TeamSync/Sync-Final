ebe3ff6bf1bd0d53f7f63be079f202634459cb17
Christopher Rios-Villalobos <christopher.rios-villalobos@yale.edu>
2014-12-08 21:14:27 -0500

    Fixed Project minus ViewVideos.java/button

--- gen/com/syncteam/sync/R.java
+++ gen/com/syncteam/sync/R.java
@@ -1081,7 +1081,7 @@ containing a value of this type.
         public static final int sync_logo=0x7f020058;
     }
     public static final class id {
-        public static final int SMSLabel=0x7f090043;
+        public static final int SMSLabel=0x7f090044;
         public static final int action_bar=0x7f09001c;
         public static final int action_bar_activity_content=0x7f090001;
         public static final int action_bar_container=0x7f09001b;
@@ -1093,22 +1093,22 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f090002;
         public static final int action_menu_presenter=0x7f090003;
         public static final int action_mode_close_button=0x7f090024;
-        public static final int action_settings=0x7f090048;
+        public static final int action_settings=0x7f090049;
         public static final int activity_chooser_view_content=0x7f090025;
         public static final int always=0x7f09000f;
         public static final int beginning=0x7f090016;
-        public static final int button=0x7f090047;
-        public static final int button1=0x7f09003e;
-        public static final int button2=0x7f09003f;
+        public static final int button=0x7f090048;
+        public static final int button1=0x7f09003d;
+        public static final int button2=0x7f09003e;
         public static final int checkbox=0x7f09002d;
         public static final int collapseActionView=0x7f090010;
         public static final int default_activity_button=0x7f090028;
         public static final int dialog=0x7f090014;
         public static final int disableHome=0x7f090009;
         public static final int dropdown=0x7f090015;
-        public static final int editPhoneNum=0x7f090042;
-        public static final int editWatchTime=0x7f090044;
-        public static final int editYouTubeLink=0x7f090046;
+        public static final int editPhoneNum=0x7f090043;
+        public static final int editWatchTime=0x7f090045;
+        public static final int editYouTubeLink=0x7f090047;
         public static final int edit_query=0x7f090030;
         public static final int end=0x7f090017;
         public static final int expand_activities_button=0x7f090026;
@@ -1118,15 +1118,15 @@ containing a value of this type.
         public static final int icon=0x7f09002a;
         public static final int ifRoom=0x7f090011;
         public static final int image=0x7f090027;
-        public static final int imageView1=0x7f09003d;
-        public static final int linearLayout1=0x7f090040;
+        public static final int linearLayout1=0x7f090041;
         public static final int listMode=0x7f090006;
+        public static final int listView1=0x7f090040;
         public static final int list_item=0x7f090029;
         public static final int middle=0x7f090018;
         public static final int never=0x7f090012;
         public static final int none=0x7f090019;
         public static final int normal=0x7f090007;
-        public static final int phoneNum=0x7f090041;
+        public static final int phoneNum=0x7f090042;
         public static final int progress_circular=0x7f090004;
         public static final int progress_horizontal=0x7f090005;
         public static final int radio=0x7f09002f;
@@ -1153,7 +1153,8 @@ containing a value of this type.
         public static final int up=0x7f090021;
         public static final int useLogo=0x7f09000e;
         public static final int withText=0x7f090013;
-        public static final int youtubeLink=0x7f090045;
+        public static final int youtubeLink=0x7f090046;
+        public static final int youtube_view=0x7f09003f;
     }
     public static final class integer {
         /**  The maximum number of action buttons that should be permitted within
@@ -1206,8 +1207,11 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_main=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
-        public static final int textsenderlayout=0x7f03001a;
+        public static final int embed_youtube=0x7f030019;
+        public static final int received_list=0x7f03001a;
+        public static final int send_videos=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int view_videos=0x7f03001d;
     }
     public static final class menu {
         public static final int text_sender=0x7f0c0000;
@@ -1255,9 +1259,13 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int enter_phone_number=0x7f0a0013;
+        public static final int enter_watch_time=0x7f0a0014;
+        public static final int enter_youtube_link=0x7f0a0015;
         public static final int hello_world=0x7f0a000e;
-        public static final int receive_video=0x7f0a0012;
+        public static final int send=0x7f0a0016;
         public static final int send_video=0x7f0a0011;
+        public static final int view_videos=0x7f0a0012;
         public static final int welcome=0x7f0a0010;
     }
     public static final class style {
--- src/com/syncteam/sync/MainActivity.java
+++ src/com/syncteam/sync/MainActivity.java
@@ -13,15 +13,19 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Intent intent = new Intent(this, YouTubeEmbed.class);
+		//startActivity(intent);
+		//Chris: I attempted to have the Youtube Video play on the home screen, but it didn't work.
 	}
-	public void gotoActivity(View v)
+	public void gotoActivitySendVideos(View v)
 	{
 		Intent intent =  new Intent(this, SendVideos.class);
 		startActivity(intent);
 	}
 	public void gotoActivityViewVideos(View v)
 	{
-		Intent intent = new Intent(this, YouTubeEmbed.class);
+		Intent intent = new Intent(this, ViewVideos.class);
 		startActivity(intent);
 	}
 }
